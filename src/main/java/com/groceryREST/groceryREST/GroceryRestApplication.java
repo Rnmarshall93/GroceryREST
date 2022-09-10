f32b1f532fa0da91c4906fd8023b6bd067712f77
Rnmarshall <rnmarshall1993@gmail.com>
2022-09-09 22:51:25 -0700

    Initial commit

---
+++ src/main/java/com/groceryREST/groceryREST/GroceryRestApplication.java
@@ -0,0 +1,13 @@
+package com.groceryREST.groceryREST;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroceryRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroceryRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/groceryREST/groceryREST/GroceryRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.groceryREST.groceryREST;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroceryRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
