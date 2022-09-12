6ccce4ac78282ed64600a261354a28e7b047935d
Rnmarshall <rnmarshall1993@gmail.com>
2022-09-11 22:48:30 -0700

    added editGroceryItem method

--- src/main/java/com/groceryREST/groceryREST/DAO/IGroceryItemDAO.java
+++ src/main/java/com/groceryREST/groceryREST/DAO/IGroceryItemDAO.java
@@ -7,7 +7,7 @@ import java.util.Set;
 public interface IGroceryItemDAO {
 
     public GroceryItem getGroceryItem(int id);
-
+    public GroceryItem editGroceryItem(GroceryItem groceryItem);
     public Set<GroceryItem> getGroceryItemsByCategory(String category);
 
 
