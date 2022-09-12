8ca52aed1c62625cf2a213553c802ce728ff10e5
Rnmarshall <rnmarshall1993@gmail.com>
2022-09-11 22:44:41 -0700

    Added IGroveryItemDAO interface

--- src/main/java/com/groceryREST/groceryREST/DAO/IGroceryItemDAO.java
+++ src/main/java/com/groceryREST/groceryREST/DAO/IGroceryItemDAO.java
@@ -1,2 +1,14 @@
-package com.groceryREST.groceryREST.DAO;public interface IGroceryItemDAO {
+package com.groceryREST.groceryREST.DAO;
+
+import com.groceryREST.groceryREST.Entity.GroceryItem;
+
+import java.util.Set;
+
+public interface IGroceryItemDAO {
+
+    public GroceryItem getGroceryItem(int id);
+
+    public Set<GroceryItem> getGroceryItemsByCategory(String category);
+
+
 }
