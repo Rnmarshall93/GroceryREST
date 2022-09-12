ff31496aa9040ed6722c8079c9a782f1a3bfde7b
Rnmarshall <rnmarshall1993@gmail.com>
2022-09-12 12:50:03 -0700

    Added Configuration bean to the bean container. DB properties are read from secretFile now instead of being explicitly read out from the hibernate.cfg.xml file

--- src/main/java/com/groceryREST/groceryREST/RestControllers/GroceryItemREST.java
+++ src/main/java/com/groceryREST/groceryREST/RestControllers/GroceryItemREST.java
@@ -2,7 +2,18 @@ package com.groceryREST.groceryREST.RestControllers;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.groceryREST.groceryREST.DAO.GroceryItemDaoImpl;
 import com.groceryREST.groceryREST.Entity.GroceryItem;
+import com.groceryREST.groceryREST.GroceryRestApplication;
+import org.hibernate.Hibernate;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,12 +23,15 @@ import java.math.RoundingMode;
 @RestController
 public class GroceryItemREST {
 
+    @Autowired
+    private ApplicationContext context;
+
     @GetMapping("/")
     public String getGroceryItem() throws JsonProcessingException {
         ObjectMapper mapper = new ObjectMapper();
 
-        GroceryItem testItem = new GroceryItem(-1, "Strawberries", "A 1LB clamshell container of strawberries",
-                "Fruit", true, new BigDecimal(12.99).setScale(3, RoundingMode.FLOOR).stripTrailingZeros());
+        GroceryItemDaoImpl groceryItemDao = context.getBean(GroceryItemDaoImpl.class);
+        GroceryItem testItem = groceryItemDao.getGroceryItem(1);
 
         return mapper.writeValueAsString(testItem);
     }
