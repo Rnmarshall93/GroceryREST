a7732bec62fff98d46d4009abb39f07ca0d07f25
Rnmarshall <rnmarshall1993@gmail.com>
2022-09-11 22:54:13 -0700

    Added hibernate annotations to members. Needs manyToMany relationship still

--- src/main/java/com/groceryREST/groceryREST/Entity/Cart.java
+++ src/main/java/com/groceryREST/groceryREST/Entity/Cart.java
@@ -1,11 +1,21 @@
 package com.groceryREST.groceryREST.Entity;
-
+import javax.persistence.*;
 import java.util.Set;
 
+
+@Table(name = "cart")
 public class Cart {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private int id;
+
+    @Column(name = "userId")
     private int userId;
+
+    //todo setup many to many relationship
+    @Transient
     private Set<GroceryItem> itemsInCart;
 
     public int getId() {
