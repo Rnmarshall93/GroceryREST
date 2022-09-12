14a2f919ea6522804ed0cc3ca8bd0747cd7320c7
Rnmarshall <rnmarshall1993@gmail.com>
2022-09-12 12:48:43 -0700

    Added GroveryItemDaoImpl class. GetGroceryItem method implemented

--- src/main/java/com/groceryREST/groceryREST/DAO/GroceryItemDaoImpl.java
+++ src/main/java/com/groceryREST/groceryREST/DAO/GroceryItemDaoImpl.java
@@ -1,1 +1,45 @@
-package com.groceryREST.groceryREST.DAO;  public class GroceryItemDaoImpl { }
+package com.groceryREST.groceryREST.DAO;
+
+import com.groceryREST.groceryREST.Entity.GroceryItem;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.stereotype.Component;
+
+import java.util.Set;
+
+@Component
+public class GroceryItemDaoImpl implements IGroceryItemDAO{
+
+    @Autowired
+    ApplicationContext context;
+
+    @Autowired Configuration configuration;
+    @Override
+    public GroceryItem getGroceryItem(int id) {
+
+        SessionFactory sessionFactory = configuration.buildSessionFactory();
+        Session session = sessionFactory.getCurrentSession();
+
+        session.beginTransaction();
+
+        GroceryItem foundItem = session.get(GroceryItem.class, id);
+
+        session.getTransaction().commit();
+        session.close();
+
+        return foundItem;
+    }
+
+    @Override
+    public GroceryItem editGroceryItem(GroceryItem groceryItem) {
+        return null;
+    }
+
+    @Override
+    public Set<GroceryItem> getGroceryItemsByCategory(String category) {
+        return null;
+    }
+}
