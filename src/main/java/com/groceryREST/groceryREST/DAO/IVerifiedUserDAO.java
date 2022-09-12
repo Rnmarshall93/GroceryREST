e5a7900c4af33db3e8eb73d147085564c8384034
Rnmarshall <rnmarshall1993@gmail.com>
2022-09-11 22:47:14 -0700

    Added IVerifiedUserDAO interface

--- src/main/java/com/groceryREST/groceryREST/DAO/IVerifiedUserDAO.java
+++ src/main/java/com/groceryREST/groceryREST/DAO/IVerifiedUserDAO.java
@@ -1,2 +1,8 @@
-package com.groceryREST.groceryREST.DAO;public interface IVerifiedUserDAO {
+package com.groceryREST.groceryREST.DAO;
+
+import com.groceryREST.groceryREST.Entity.VerifiedUser;
+
+public interface IVerifiedUserDAO {
+    public String GetUser(String username, String password);
+    public String UpdateUser(VerifiedUser user);
 }
