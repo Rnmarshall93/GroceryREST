5a91ac30d28e2c9a419455fbfe5685a91d4bc30b
Rnmarshall <rnmarshall1993@gmail.com>
2022-09-13 10:40:13 -0700

    Added two methods for deleteCart.

--- src/main/java/com/groceryREST/groceryREST/DAO/ICartDAO.java
+++ src/main/java/com/groceryREST/groceryREST/DAO/ICartDAO.java
@@ -6,4 +6,6 @@ public interface ICartDAO {
     public Cart getCart(int cardId);
     public void updateCart(Cart cart);
     public void addCart(Cart cart);
+    public void deleteCart(int cartId);
+    public void deleteCart(Cart cart);
 }
