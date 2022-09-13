8eac3361a2f6d3cb350b4e1bf17bc0ddd22eac40
Rnmarshall <rnmarshall1993@gmail.com>
2022-09-13 14:29:01 -0700

    Changed method signatures for GetUser and UpdateUser

--- src/main/java/com/groceryREST/groceryREST/DAO/IVerifiedUserDAO.java
+++ src/main/java/com/groceryREST/groceryREST/DAO/IVerifiedUserDAO.java
@@ -3,6 +3,6 @@ package com.groceryREST.groceryREST.DAO;
 import com.groceryREST.groceryREST.Entity.VerifiedUser;
 
 public interface IVerifiedUserDAO {
-    public String GetUser(String username, String password);
-    public String UpdateUser(VerifiedUser user);
+    public VerifiedUser GetUser(String username, String password);
+    public void UpdateUser(VerifiedUser user);
 }
