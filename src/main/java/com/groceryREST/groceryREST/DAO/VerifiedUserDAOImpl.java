00925ab8e5015a3df61905d2190ba1dc360cfe4d
Rnmarshall <rnmarshall1993@gmail.com>
2022-09-13 13:02:50 -0700

    Added implementation for IVerifiedUserDAO with method stubs

---
+++ src/main/java/com/groceryREST/groceryREST/DAO/VerifiedUserDAOImpl.java
@@ -0,0 +1,15 @@
+package com.groceryREST.groceryREST.DAO;
+
+import com.groceryREST.groceryREST.Entity.VerifiedUser;
+
+public class VerifiedUserDAOImpl implements IVerifiedUserDAO{
+    @Override
+    public String GetUser(String username, String password) {
+        return null;
+    }
+
+    @Override
+    public String UpdateUser(VerifiedUser user) {
+        return null;
+    }
+}
